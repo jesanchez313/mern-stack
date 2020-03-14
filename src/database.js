ace15720b94f9eb42ab0f46474712e31765ee25a
Jeferson Esteban Sanchez <jeferson.sanchez@atlanticsoft.us>
2020-03-14 15:07:18 -0500

    script start to heroku

--- src/database.js
+++ src/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://db:27017/mernstack";
+// mongodb://heroku_5qxngcw3:as0fk6o9f74lc7kv54ksjqhmcs@ds155674.mlab.com:55674/heroku_5qxngcw3
+// mongodb://db:27017/mernstack
+const URI =
+  "mongodb://heroku_5qxngcw3:as0fk6o9f74lc7kv54ksjqhmcs@ds155674.mlab.com:55674/heroku_5qxngcw3";
 
 const connect = mongoose.connect(URI, {
   useNewUrlParser: true,
