3bbaa8f67a6d4c4e66a0002411fd7db1a502e628
Jeferson Esteban Sanchez <jeferson.sanchez@atlanticsoft.us>
2020-03-14 22:25:45 -0500

    config server to client html

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 
+//Config
 app.set("port", process.env.PORT || 8080);
 
+//Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(__dirname + "/../client/build"));
 
+//routes
 app.use("/api/profiles", require("./routes/profiles"));
-
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(__dirname + "/../client/build/index.html");
 });
 
 module.exports = app;
