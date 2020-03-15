01aaf7f10c9e44a21739432d411f3e365ecd3b4f
Jeferson Esteban Sanchez <jeferson.sanchez@atlanticsoft.us>
2020-03-15 09:40:13 -0500

    fix heroku route

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // app.use(express.static(__dirname + "/../client/build"));
 
 //routes
-app.use("/api/profiles", require("routes/profiles"));
+app.use("/api/profiles", require("./routes/profiles"));
 // app.get((req, res) => {
 //   res.sendFile(__dirname + "/../client/build/index.html");
 // });
