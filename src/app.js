2c4f412d02ebda6cd4917d7f7499e37534898c94
Jeferson Esteban Sanchez <jeferson.sanchez@atlanticsoft.us>
2020-03-14 22:52:01 -0500

    test api

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 //Config
-app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 8000);
 
 //Middleware
 app.use(cors());
@@ -13,8 +13,5 @@ app.use(express.static(__dirname + "/../client/build"));
 
 //routes
 app.use("/api/profiles", require("./routes/profiles"));
-app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/../client/build/index.html");
-});
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ require("./database");
 
 const port = app.get("port");
 
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/../client/build/index.html");
+});
+
 const main = async () => {
   await app.listen(port);
   console.log("runing port " + port);
